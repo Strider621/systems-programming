65a50f227a49f0e713c66ea4f91fd3ff9e58d0db
Segundo Rios <ferdex.psn@gmail.com>
2025-07-26 23:44:11 +0000

    Did part two of assigment 1

--- Module_1_Assigment/src/main.rs
+++ Module_1_Assigment/src/main.rs
@@ -1,4 +1,3 @@
-
 fn temp_converter(){
 
     const freezePoint:f64 = 32.0;
@@ -24,17 +23,56 @@ fn temp_converter(){
 
 }
 
-fn main() {
-    temp_converter();
+fn number_analyzer(){
+
+    fn is_even(n:i32) -> bool{
+        n % 2 == 0
+    }
+    
+    let numbers_arr = [1,2,3,5,10,13,15,25];
+
+    
+    for &num in numbers_arr.iter() {
+
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("{} FizzBuzz", num);
+        } else if num % 3 == 0 {
+            println!("{} Fizz", num);
+        } else if num % 5 == 0 {
+            println!("{} Buzz", num);
+        } else {
+            
+            if is_even(num) {
+                println!("{} is even", num);
+            } else {
+                println!("{} is odd", num);
+            }
+        }
+    }
+
+    let mut sum = 0;
+    let mut i = 0;
+    while i < numbers_arr.len() {
+        sum += numbers_arr[i];
+        i += 1;
+    }
+    println!("The sum of numbers in tha array is : {}", sum);
+
+    let mut largest = numbers_arr[0];
+    for &num in numbers_arr.iter() {
+        if num > largest {
+            largest = num;
+        }
+    }
+    println!("The largest number in array is : {}", largest);
+
+
 }
 
-// Create a Rust program that converts temperatures between Fahrenheit and Celsius. The program should:
 
-// 1. Declare a constant for the freezing point of water in Fahrenheit (32°F).
-// 2. Implement two functions:
-//    - `fahrenheit_to_celsius(f: f64) -> f64`: Converts Fahrenheit to Celsius
-//    - `celsius_to_fahrenheit(c: f64) -> f64`: Converts Celsius to Fahrenheit
-// 3. In the main function:
-//    - Declare a mutable variable with a temperature in Fahrenheit
-//    - Convert it to Celsius using your function and print the result
-//    - Use a loop to convert and print the next 5 integer temperatures (e.g., if you start with 32°F, print conversions for 33°F, 34°F, 35°F, 36°F, and 37°F)
+fn main() {
+    //temp_converter();
+    //number_analyzer();
+}
+
+
